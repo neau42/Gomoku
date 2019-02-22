e693dfafd698e07bfb1dcc98c3cf585f43abe4d3
no <no@42.fr>
2019-02-22 23:57:37 +0100

    test print text

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
 
 	//Set Text Style
 	let texture_settings = TextureSettings::new().filter(Filter::Nearest);
-	let ref mut glyphs = GlyphCache::new("assets/FiraSans-Regular.ttf", (), texture_settings);
+	let ref mut glyphs = GlyphCache::new("assets/FiraSans-Regular.ttf", (), texture_settings)
+		.expect("Could not load font");;
 
 	let mut gl = GlGraphics::new(opengl);
 	let mut events = Events::new(EventSettings::new().lazy(true));
@@ -23,17 +24,15 @@ fn main() {
 	while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
 			let draw_closure = |context: Context, graphic: &mut GlGraphics| {
-				// let transform = context.transform.trans(10.0, 100.0);
-				// clear(fill_color(195, 155, 95, 1.0), graphic);
-				// graphic.clear_stencil(0);
-
-				// text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
-				// 	"Hello world!",
-				// 	&mut glyphs,
-				// 	&context.draw_state,
-				// 	transform, graphic
-				// ).unwrap();
+				let transform = context.transform.trans(10.0, 100.0);
 				clear(fill_color(195, 155, 95, 1.0), graphic);
+				graphic.clear_stencil(0);
+				text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
+					"Hello world!",
+					glyphs,
+					&context.draw_state,
+					transform, graphic
+				).unwrap();
 			};
             gl.draw(args.viewport(), draw_closure);
 		}
