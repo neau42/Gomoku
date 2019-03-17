e3ae7828a33c891724e62e24c1809d822538131c
pacifistes <bbrunell@student.42.fr>
2019-03-17 23:39:53 +0100

    push du jour

--- src/models/gameboard.rs
+++ src/models/gameboard.rs
@@ -43,23 +43,11 @@ impl Gameboard {
 }
 
 impl Gameboard {
-    pub fn make_move(&mut self, x: usize, y: usize, stone: Stone) -> bool {
-		if self.cells[x][y] == Stone::NOPE {
-			    self.update_possible_move(x, y);
-				self.cells[x][y] = stone;
-                return true;
-        }
-        false
-    }
-
 	pub fn eval(&self) -> isize {
 		0
 	}
 
-    pub fn unmake_move(&mut self, x: usize, y: usize) {
-        self.cells[x][y] = Stone::NOPE;
-    }
-	
+
 	pub fn update_possible_move(&mut self, x: usize, y: usize) {
         let directions: [(isize, isize); 8] = [(0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)];
         directions.iter().for_each(|(tmp_x, tmp_y)| {
@@ -126,4 +114,86 @@ impl Gameboard {
 }
 
 impl Gameboard {
+	pub fn make_move(&mut self, x: usize, y: usize, stone: Stone) -> bool {
+		if self.cells[x][y] == Stone::NOPE && self.check_double_tree(x, y, stone) {
+				let nbr_capture = apply_capture();
+				self.cells[x][y] = stone;
+			    self.update_possible_move(x, y);
+                return true;
+        }
+        false
+    }
+
+    pub fn unmake_move(&mut self, x: usize, y: usize) {
+        self.cells[x][y] = Stone::NOPE;
+    }
+
+}
+
+impl Gameboard {
+    // True if capture is possible
+    pub fn check_capture(&self, x: usize, y: usize, actual_stone: Stone) -> bool {
+        let directions: [(isize, isize); 8] = [(0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)];
+
+        directions.iter().any(|(tmp_x, tmp_y)| {
+            (1..=3 as isize).all(|i| {
+                let tmp_x = *tmp_x  * i + x as isize;
+                let tmp_y = *tmp_y * i + y as isize;
+                if tmp_x < 0 || tmp_x >= self.size as isize || tmp_y < 0 || tmp_y >= self.size as isize {
+                    return false;
+                }
+                let tmp_stone = self.cells[tmp_x as usize][tmp_y as usize];
+                match i {
+                    1 | 2 => tmp_stone != actual_stone && tmp_stone != Stone::NOPE,
+                    _ => tmp_stone == actual_stone,
+                }
+            })
+        })
+	}
+
+	pub fn check_double_tree(&self, x: usize, y: usize, actual_stone: Stone) -> bool {
+        let directions: [(isize, isize); 4] = [(0,1), (1,0), (1,1), (1,-1)];
+
+        let closure = |tmp_x: isize, tmp_y: isize| -> Vec<Stone> {
+            (1..=5 as isize).filter_map(|i| {
+                let tmp_x = tmp_x  * i + x as isize;
+                let tmp_y = tmp_y * i + y as isize;
+                if tmp_x < 0 || tmp_x >= self.size as isize || tmp_y < 0 || tmp_y >= self.size as isize {
+                    return None;
+                }
+                Some(self.cells[tmp_x as usize][tmp_y as usize])
+            }).collect()
+        };
+        
+        let nbr_tree = directions.iter().fold(0, |nbr_tree, (tmp_x, tmp_y)| {
+            let right_side = closure(*tmp_x, *tmp_y);
+            let mut left_side = closure(tmp_x * -1, tmp_y * -1);
+            left_side.reverse();
+            let line = [&left_side[..], &vec![actual_stone][..], &right_side[..]].concat();
+            let len = line.len();
+            if len < 6 {
+                return nbr_tree;
+            }
+            let is_tree: bool = (0..=(len - 6)).any(|i| {
+                line[i] == Stone::NOPE
+                && line[i + 5] == Stone::NOPE
+                && line[(i + 1)..(i + 5)].iter()
+                .fold(0, |sum, stone| {
+                    match *stone {
+                        otherstone if otherstone == actual_stone => sum + 1,
+                        Stone::NOPE => sum + 2,
+                        _ => sum + 3,
+                    }
+                }) == 5
+            });
+            if is_tree {
+                nbr_tree + 1
+            }
+            else {
+                nbr_tree
+            }
+        });
+        println!("nbr_tree = {}", nbr_tree);
+        nbr_tree >= 2
+	}
 }
